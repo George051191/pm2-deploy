1f8637ee47e71c38a174aa806ac6c68af3bf2d52
George051191 <trubacheff_91@mail.ru>
2022-10-12 12:16:03 +0300

    fixes

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
     deploy: {
         production: {
             user: DEPLOY_USER,
-            key: '~/.ssh/id_rsa',
+            ssh_options: "StrictHostKeyChecking=no",
             host: DEPLOY_HOST,
             ref: DEPLOY_REF,
             repo: 'https://github.com/George051191/pm2-deploy',
